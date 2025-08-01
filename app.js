4f93574f74515b3ac6b30be6765dedeb24f22311
Michael Hall <128835449+SocraticTactic@users.noreply.github.com>
2025-08-01 11:13:40 -0500

    Initial rebuild

---
+++ app.js
@@ -0,0 +1,19 @@
+//* Navbar JS *//
+const menu = document.getElementById("mobile-menu");
+const navMenu = document.querySelector(".navbar-menu");
+const navLinks = document.querySelectorAll(".navbar-links");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("change");   // animate hamburger to X
+  navMenu.classList.toggle("active"); // slide down menu
+});
+
+// Close menu when any link is clicked
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    if (navMenu.classList.contains("active")) {
+      navMenu.classList.remove("active");
+      menu.classList.remove("change");
+    }
+  });
+});
