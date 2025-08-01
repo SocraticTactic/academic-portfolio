5f5bbe13cce644c7650f3d630ca0a29f8c3af0a1
Michael Hall <128835449+SocraticTactic@users.noreply.github.com>
2025-08-01 11:20:08 -0500

    Prettier formatting

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const navMenu = document.querySelector(".navbar-menu");
 const navLinks = document.querySelectorAll(".navbar-links");
 
 menu.addEventListener("click", () => {
-  menu.classList.toggle("change");   // animate hamburger to X
+  menu.classList.toggle("change"); // animate hamburger to X
   navMenu.classList.toggle("active"); // slide down menu
 });
 
 // Close menu when any link is clicked
-navLinks.forEach(link => {
+navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     if (navMenu.classList.contains("active")) {
       navMenu.classList.remove("active");
